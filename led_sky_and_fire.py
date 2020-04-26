89b5987d876316ecf3373723ce9e1cd840221acd
Elwetana <martin@argenite.org>
2020-04-26 20:02:39 +0100

    Changed number of LEDs

--- led_sky_and_fire.py
+++ led_sky_and_fire.py
@@ -42,7 +42,7 @@ class Ember:
 
 class App:
     # LED strip configuration:
-    N_LEDS = 150       # Number of LED pixels.
+    N_LEDS = 454          # Number of LED pixels.
     LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
     # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
     LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
