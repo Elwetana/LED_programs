44e5428898fda2860ece220ffc9ddd45b68c1ded
Martin Klima <martin@argenite.org>
2020-04-28 22:09:54 +0200

    * Optimizations

--- fire_source.py
+++ fire_source.py
@@ -2,7 +2,7 @@
 
 from basic_source import BasicSource
 from random import random
-from math import cos, pi, exp
+from math import cos, pi, exp, fabs
 
 
 class Ember:
@@ -18,23 +18,45 @@ class Ember:
         self.decay = ember_data["decay"] + ember_data["decay_rand"] * random()
         self.age = age
         self.type = ember_type
-        if ember_type == "big":
-            print(self.x, self.amp)
+        self.cos_table = []
+        self.contrib_table = []
+        t = 0
+        while t * self.osc_freq < 2 * pi:
+            self.cos_table.append(cos(t * self.osc_freq + self.osc_shift))
+            contrib = []
+            x = -6 * self.sigma
+            while x < 6 * self.sigma:
+                osc = self.osc_amp * self.cos_table[t]
+                c = (self.amp + osc) * exp(-0.5 * ((x / self.sigma * self.amp / (self.amp + osc)) ** 2))
+                contrib.append(c)
+                x += 1
+            self.contrib_table.append(contrib)
+            t += 1
+
 
     def get_contrib(self, x, t):
-        osc = self.osc_amp * cos(self.osc_freq * t + self.osc_shift)
-        return (self.amp + osc) * \
-            exp(-0.5 * ((self.x - x) * self.amp / self.sigma / (self.amp + osc))**2 - self.decay * (self.age - t)**2)
+        """
+        c = A' * e^ 1/2 ((x - x0)/sig)^2
+        :param x:
+        :param t:
+        :return:
+        """
+        cos_t = t % len(self.cos_table)
+        dx = int(self.x - x + 6 * self.sigma)
+        if self.decay == 0:
+            return self.contrib_table[cos_t][dx]
+        else:
+            return self.contrib_table[cos_t][dx] * exp(-0.5 * self.decay * (self.age - t)**2)
 
 
 class FireSource(BasicSource):
 
     EMBERS = {  # x_space = N_LEDS / (count - 2)
-        "big": {"amp": 0.4, "amp_rand": 0.1, "x_space": 50, "sigma": 15, "sigma_rand": 2,
+        "big": {"amp": 0.4, "amp_rand": 0.1, "x_space": 100, "sigma": 30, "sigma_rand": 2,
                 "osc_amp": 0.2, "osc_freq": 0.005, "osc_freq_rand": 0.01, "decay": 0.0, "decay_rand": 0},
-        "small": {"amp": 0.2, "amp_rand": 0.05, "x_space": 35, "sigma": 5, "sigma_rand": 2,
+        "small": {"amp": 0.2, "amp_rand": 0.05, "x_space": 60, "sigma": 9, "sigma_rand": 2,
                   "osc_amp": 0.2, "osc_freq": 0.01, "osc_freq_rand": 0.005, "decay": 0.0, "decay_rand": 0},
-        "spark": {"amp": 0.1, "amp_rand": 0.2, "x_space": 15, "sigma": 2, "sigma_rand": 1,
+        "spark": {"amp": 0.1, "amp_rand": 0.2, "x_space": 25, "sigma": 3, "sigma_rand": 1,
                   "osc_amp": 0.2, "osc_freq": 0.01, "osc_freq_rand": 0.01, "decay": 0.001, "decay_rand": 0.001}
     }
 
@@ -66,12 +88,14 @@ class FireSource(BasicSource):
                                        frame + 100 + 100 * random(), "spark")
 
     def get_values(self, frame):
-        self.update_embers(self.time_speed * frame)
+        if frame % 4 == 0:
+            self.update_embers(self.time_speed * frame)
         values = []
         for i in range(self.nLed):
             y = 0
             for e in self.embers:
-                y += e.get_contrib(i, self.time_speed * frame)
+                if fabs(i - e.x) < 6 * e.sigma:
+                    y += e.get_contrib(i, self.time_speed * frame)
             if y > 1:
                 y = 1.0
             values.append(BasicSource.gain(y, 0.25))
