250b8709310a83c5c76659bb45aab7f18bd3669b
Elwetana <martin@argenite.org>
2020-05-04 21:16:48 +0200

    + Added info about system state

--- http_server.py
+++ http_server.py
@@ -20,7 +20,11 @@ class LEDHttpHandler(BaseHTTPRequestHandler):
             return
         if self.path == "/":
             f = open('http/index.html', 'r', encoding="utf-8")
-            self.wfile.write(f.read().encode())
+            s = f.read()
+            if sys.platform == "linux":
+                sysinfo = LEDHttpHandler.get_sys_info()
+                s.replace("{{sysinfo}}", sysinfo)
+            self.wfile.write(s.encode())
             return
         if self.path[0:7] == "/source":
             msg = "LED SOURCE %s" % (self.path[len("/source/"):]).upper()
@@ -35,6 +39,16 @@ class LEDHttpHandler(BaseHTTPRequestHandler):
         else:
             self.wfile.write("FILE NOT FOUND".encode())
             logger.warning("File not found: %s" % fname)
+
+    @staticmethod
+    def get_sys_info():
+        fproc = open("/proc/loadavg")
+        proc = fproc.readline().split(" ")
+        proc_info = "<tr><td colspan=3>CPU Load</td></tr>\n"
+        proc_info += "<tr><td>" + "<td></td>".join(proc[0:2]) + "</td></tr>\n"
+        ftemp = open("/sys/class/thermal/thermal_zone0/temp")
+        temp_info = "<tr><td colspan=3>CPU temperature: %s°C</td></tr>\n" % (int(ftemp.readline()) / 1000)
+        return "<table>\n" + proc_info + temp_info + "</table>\n"
 
 
 class LEDHttpServer():
